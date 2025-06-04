60a2b17b1d4c64e639a2ae3bff72c642943163ce
A. Ross Morton <arossco@ntlworld.com>
2025-06-04 18:15:31 +0100

    Swift 2 buttons

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,20 +15,34 @@ struct ContentView: View {
                 
         VStack {
             
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            
+                .frame(width: 200, height: 200)
+                        
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-             
-            Button("Click Me!") {
-                message = "Awesome!"
-                print(message)
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            
+            Spacer()
+                
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                    print(message)
+                }
+                Button("Great!") {
+                    message = "Great!"
+                    print(message)
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
