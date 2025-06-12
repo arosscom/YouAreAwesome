bdc763c3aa6abdb57bcee3b2eb66f401da6ccf11
A. Ross Morton <arossco@ntlworld.com>
2025-06-12 15:49:21 +0100

    Added sounds

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,22 +6,47 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     
-    @State private var messages = ["You are Awesome!", "You are Great!",
-                                   "Fabulous? That's You!", "Sum Maximus !",
+    @State private var messages = ["You are Awesome!",
+                                   "When the Genius Bar needs help, it calls on you!",
+                                   "You are Great!",
+                                   "Fabulous? That's You!",
+                                   "Sum Maximus !",
                                    "I am the Greatest !"]
     @State private var message = ""
     
     @State private var buttonText = "Show Message"
     @State private var imageName = ""
+
+    @State private var messageNumber = 0
+    @State private var lastMessageNumber = -1
     @State private var imageNumber = 0
+    @State private var lastImageNumber = -1
+    
+    @State private var soundName = ""
+    @State private var soundNumber = 0
+    @State private var lastSoundNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!                         // implictly unwrapping an optional - must have a value when used
+    
+    let numberOfImages = 10
+    let numberOfSounds = 6
     
     var body: some View {
         
         VStack {
             
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+
             Spacer()
             
             Image(imageName)
@@ -29,25 +54,47 @@ struct ContentView: View {
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
- 
+                .animation(.default, value: imageName)
+
             Spacer()
             
             Button(buttonText) {
-                message = messages[imageNumber]
+                
+                lastMessageNumber = messageNumber
+                repeat {
+                    messageNumber = Int.random(in: 0..<messages.count)
+                    print("Message \(messageNumber)")
+                } while (messageNumber == lastMessageNumber)
+                message = messages[messageNumber]
+                
+                lastImageNumber = imageNumber
+                repeat {
+                    imageNumber = Int.random(in: 0...(numberOfImages-1))
+                    print("Image \(imageNumber)")
+                } while (imageNumber == lastImageNumber)
                 imageName = "image\(imageNumber)"
                 
-                imageNumber += 1
-                if imageNumber ==  messages.count {
-                    imageNumber = 0
+                lastSoundNumber = soundNumber
+                repeat {
+                    soundNumber = Int.random(in: 0...(numberOfSounds-1))
+                    print("Sound \(soundNumber)")
+                } while (soundNumber == lastSoundNumber)
+                soundName = "sound\(soundNumber)"
+               
+                guard let soundFile = NSDataAsset(name: soundName) else {
+                    print( "⚠️ Sound file not found \(soundName)")
+                    return
+                }
+                
+                do {
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.play()
+                } catch {
+                    print( "⚠️ Error \(error.localizedDescription)")
                 }
              }
-            .buttonStyle(.borderedProminent)
-            .font(.title2)
+             .buttonStyle(.borderedProminent)
+             .font(.title2)
 
         }
         .padding()
